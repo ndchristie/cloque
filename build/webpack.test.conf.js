b79989214ba9d34d36bb819f86846c5d7704258f
Nathan Christie <ndchristie@gmail.com>
2017-02-25 20:20:14 -0800

    clean up and configure build, test scripts

--- build/webpack.test.conf.js
+++ build/webpack.test.conf.js
@@ -1,24 +1,25 @@
 // This is the webpack config used for unit tests.
 
-var utils = require('./utils')
-var webpack = require('webpack')
-var merge = require('webpack-merge')
-var baseConfig = require('./webpack.base.conf')
+const utils = require('./utils');
+const webpack = require('webpack');
+const merge = require('webpack-merge');
+const baseConfig = require('./webpack.base.conf');
+const testEnv = require('../config/test.env');
 
-var webpackConfig = merge(baseConfig, {
+const webpackConfig = merge(baseConfig, {
   // use inline sourcemap for karma-sourcemap-loader
   module: {
-    rules: utils.styleLoaders()
+    rules: utils.styleLoaders(),
   },
   devtool: '#inline-source-map',
   plugins: [
     new webpack.DefinePlugin({
-      'process.env': require('../config/test.env')
-    })
-  ]
-})
+      'process.env': testEnv,
+    }),
+  ],
+});
 
 // no need for app entry during tests
-delete webpackConfig.entry
+delete webpackConfig.entry;
 
-module.exports = webpackConfig
+module.exports = webpackConfig;
